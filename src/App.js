6b7960f6cf0d308f0fb1d2563a8e84336b1990df
RoyRee <rahulshrirame6@gmail.com>
2021-05-08 23:36:49 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hey Rahull ..I have added feature branch.
+          RoyRee is back
         </p>
         <a
           className="App-link"
